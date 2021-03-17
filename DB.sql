d6d595025983abc92a1d5d40bd04c79224660303
surya-658 <59365553+surya-658@users.noreply.github.com>
2021-03-17 22:30:55 +0530

    Create DB.sql

---
+++ DB.sql
@@ -0,0 +1,23 @@
+CREATE TABLE customers (
+customer_name  varchar(10) NOT NULL UNIQUE PRIMARY KEY ,
+customer_user_name  varchar(6) NOT NULL ,
+customer_email varchar(20),
+current_balance varchar(20) ,
+customer_address varchar(50),
+customer_aadhar varchar(12),
+customer_pan varchar(10)
+
+);
+
+INSERT INTO customers VALUES( 'pulkith' , 'eva adams ' , 'pulkithjasthi69@gmail.com ' , '2' , 'wertyuipkhf' , '123455' , 'PAN123456');
+INSERT INTO customers VALUES( 'harshad ' , 'blooger' , 'harshadhane@gmail.com ' , '3456789' , 'qwertyupkjhgf' , '09923467' , 'PAN123');
+INSERT INTO customers VALUES( 'harshad ' , 'blooger' , 'harshadhane@gmail.com ' , '3456789' , 'qwertyupkjhgf' , '09923467' , 'PAN123');
+INSERT INTO customers VALUES( 'harshad ' , 'blooger' , 'harshadhane@gmail.com ' , '3456789' , 'qwertyupkjhgf' , '09923467' , 'PAN123');
+INSERT INTO customers VALUES( 'harshad ' , 'blooger' , 'harshadhane@gmail.com ' , '3456789' , 'qwertyupkjhgf' , '09923467' , 'PAN123');
+INSERT INTO customers VALUES( 'harshad ' , 'blooger' , 'harshadhane@gmail.com ' , '3456789' , 'qwertyupkjhgf' , '09923467' , 'PAN123');
+INSERT INTO customers VALUES( 'harshad ' , 'blooger' , 'harshadhane@gmail.com ' , '3456789' , 'qwertyupkjhgf' , '09923467' , 'PAN123');
+INSERT INTO customers VALUES( 'harshad ' , 'blooger' , 'harshadhane@gmail.com ' , '3456789' , 'qwertyupkjhgf' , '09923467' , 'PAN123');
+
+
+SELECT *
+FROM customers ;
